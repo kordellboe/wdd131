83361fbae8aa4f88c82942ed4097f988603e3545
kordellboe <kordellboehning1@gmail.com>
2025-09-23 10:29:53 -0600

    final

--- ponder/meta.css.js
+++ ponder/meta.css.js
@@ -8,3 +8,26 @@ radius = 20;
 
 area = radius * radius * PI;
 console.log(area);
+
+
+const one = 1;
+const two = '2';
+                    
+letresult = one + Number(two); 
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+})
+                
