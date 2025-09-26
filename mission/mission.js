95773803ecd47e2e234259bf8eef3ae70d7136d4
kordellboe <kordellboehning1@gmail.com>
2025-09-25 22:13:05 -0700

    mobile

--- mission/mission.js
+++ mission/mission.js
@@ -7,12 +7,12 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-            document.body.style.backgroundColor = 'black';
-            document.body.style.color = 'white';
+            document.body.style.backgroundColor = '#201f1fff';
+            document.body.style.color = '#ffffffcc';
             logo.src = 'byui-logo-white.png';
     } else {
-            document.body.style.backgroundColor = 'white';
-            document.body.style.color = 'black';
+            document.body.style.backgroundColor = '#d7ecf6ff'; // I used # instead of 'white' to get pratice 
+            document.body.style.color = '#000000ff';
             logo.src = 'byui-logo-blue.webp';
     }
 }           
